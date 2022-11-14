b3a6af0703d7a73e864f4ecaf9e897ed4c29df8a
Damian Dudka <dudka@sas.upenn.edu>
2022-11-14 13:57:49 -0500

    Modified make.bat and Makefile; added githubpages extension

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['myst_parser', 'sphinx.ext.autosectionlabel']
+extensions = ['myst_parser', 'sphinx.ext.autosectionlabel', "sphinx.ext.githubpages"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
